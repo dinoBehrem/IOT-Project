7563eaa756a20ebda9743ada0df5e23569921aba
dinoBehrem <dinobehrem23@gmail.com>
2024-08-30 10:00:05 +0200

    Added dashboard

--- main.js
+++ main.js
@@ -1,78 +1,137 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyB8vfdak77CGScERGh2Y7AQHNeYVXPti8c",
-    authDomain: "iot-project-93c00.firebaseapp.com",
-    databaseURL: "https://iot-project-93c00-default-rtdb.firebaseio.com",
-    projectId: "iot-project-93c00",
-    storageBucket: "iot-project-93c00.appspot.com",
-    messagingSenderId: "808237092699",
-    appId: "1:808237092699:web:6ceec790c5e8460019147e",
-    measurementId: "G-R0ZB3KHZHE"
+  apiKey: "AIzaSyB8vfdak77CGScERGh2Y7AQHNeYVXPti8c",
+  authDomain: "iot-project-93c00.firebaseapp.com",
+  databaseURL: "https://iot-project-93c00-default-rtdb.firebaseio.com",
+  projectId: "iot-project-93c00",
+  storageBucket: "iot-project-93c00.appspot.com",
+  messagingSenderId: "808237092699",
+  appId: "1:808237092699:web:6ceec790c5e8460019147e",
+  measurementId: "G-R0ZB3KHZHE",
 };
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+let firestore = firebase.firestore();
+
 //firebase data
-let pump_1 = firebase.database().ref("pump_1");
-let pump_2 = firebase.database().ref("pump_2");
-let pump_3 = firebase.database().ref("pump_3");
-let pump_4 = firebase.database().ref("pump_4");
+let pump_1 = firebase.database().ref("pumpe/pump1");
+let pump_2 = firebase.database().ref("pumpe/pump2");
+let pump_3 = firebase.database().ref("pumpe/pump3");
+let pump_4 = firebase.database().ref("pumpe/pump4");
 
-let temp_1 = 0;
-let temp_2 = 0;
+pump_1.on("value", function (snapshot) {
+  var dataArray = snapshot.val();
+  console.log("Pump 1 Data:", dataArray);
 
-let active_pump1;
+  const tempElement = document.querySelectorAll(".fa-thermometer-half")[0];
+  const timeElement = document.querySelectorAll(".fa-stopwatch")[0];
+  const isMalfunctioning = document.querySelectorAll(".fa-ban")[0];
+  const pumpImg = document.querySelectorAll("img")[0];
 
-pump_1.on('value', data => {
-    document.querySelectorAll(".fa-thermometer-half")[0].innerText = data.val().temp + '°C';
-    document.querySelectorAll(".fa-stopwatch")[0].innerText = convertTime(data.val().active_time);
+  if (tempElement && timeElement && isMalfunctioning && pumpImg) {
+    tempElement.innerText = dataArray.temperature + "°C";
+    timeElement.innerText = convertTime(dataArray.activeHorus);
+    isMalfunctioning.setAttribute(
+      "style",
+      dataArray.isMalfunctioning ? "color:red" : "color:green"
+    );
 
-    if (!data.val().isWorking && !data.val().broken && data.val().temp <= 60 && data.val().active_time <= 50) {
-        active_pump1 = data; 
-    }else{
-        data.isWorking = true;
-    }
+    pumpImg.setAttribute(
+      "style",
+      dataArray.isActive ? "background-color:green" : "background-color:yellow"
+    );
+  } else {
+    console.error("Elements for Pump 1 not found");
+  }
 });
 
-pump_2.on('value', data => {
-    document.querySelectorAll(".fa-thermometer-half")[1].innerText = data.val().temp + '°C';
-    document.querySelectorAll(".fa-stopwatch")[1].innerText = convertTime(data.val().active_time);
+pump_2.on("value", (snapshot) => {
+  var dataArray = snapshot.val();
+  console.log("Pump 1 Data:", dataArray);
+
+  const tempElement = document.querySelectorAll(".fa-thermometer-half")[1];
+  const timeElement = document.querySelectorAll(".fa-stopwatch")[1];
+  const isMalfunctioning = document.querySelectorAll(".fa-ban")[1];
+  const pumpImg = document.querySelectorAll("img")[1];
+
+  if (tempElement && timeElement && isMalfunctioning && pumpImg) {
+    tempElement.innerText = dataArray.temperature + "°C";
+    timeElement.innerText = convertTime(dataArray.activeHours);
+    isMalfunctioning.setAttribute(
+      "style",
+      dataArray.isMalfunctioning ? "color:red" : "color:green"
+    );
+
+    pumpImg.setAttribute(
+      "style",
+      dataArray.isActive ? "background-color:green" : "background-color:yellow"
+    );
+  } else {
+    console.error("Elements for Pump 2 not found");
+  }
 });
 
-pump_3.on('value', data => {
-    document.querySelectorAll(".fa-thermometer-half")[2].innerText = data.val().temp + '°C';
-    document.querySelectorAll(".fa-stopwatch")[2].innerText = convertTime(data.val().active_time);
+pump_3.on("value", (snapshot) => {
+  var dataArray = snapshot.val();
+  console.log("Pump 1 Data:", dataArray);
+
+  const tempElement = document.querySelectorAll(".fa-thermometer-half")[2];
+  const timeElement = document.querySelectorAll(".fa-stopwatch")[2];
+  const isMalfunctioning = document.querySelectorAll(".fa-ban")[2];
+  const pumpImg = document.querySelectorAll("img")[2];
+
+  if (tempElement && timeElement && isMalfunctioning && pumpImg) {
+    tempElement.innerText = dataArray.temperature + "°C";
+    timeElement.innerText = convertTime(dataArray.activeHours);
+    isMalfunctioning.setAttribute(
+      "style",
+      dataArray.isMalfunctioning ? "color:red" : "color:green"
+    );
+
+    pumpImg.setAttribute(
+      "style",
+      dataArray.isActive ? "background-color:green" : "background-color:yellow"
+    );
+  } else {
+    console.error("Elements for Pump 3 not found");
+  }
 });
 
-pump_4.on('value', data => {
-    document.querySelectorAll(".fa-thermometer-half")[3].innerText = data.val().temp;
-    document.querySelectorAll(".fa-stopwatch")[3].innerText = convertTime(data.val().active_time);
+pump_4.on("value", (snapshot) => {
+  var dataArray = snapshot.val();
+  console.log("Pump 1 Data:", dataArray);
+
+  const tempElement = document.querySelectorAll(".fa-thermometer-half")[3];
+  const timeElement = document.querySelectorAll(".fa-stopwatch")[3];
+  const isMalfunctioning = document.querySelectorAll(".fa-ban")[3];
+  const pumpImg = document.querySelectorAll("img")[3];
+
+  if (tempElement && timeElement && isMalfunctioning && pumpImg) {
+    tempElement.innerText = dataArray.temperature + "°C";
+    timeElement.innerText = convertTime(dataArray.activeHours);
+    isMalfunctioning.setAttribute(
+      "style",
+      dataArray.isMalfunctioning ? "color:red" : "color:green"
+    );
+
+    pumpImg.setAttribute(
+      "style",
+      dataArray.isActive ? "background-color:green" : "background-color:yellow"
+    );
+  } else {
+    console.error("Elements for Pump 3 not found");
+  }
 });
-
-// events
-
-let incrementor_1 = 1;
-let incrementor_2 = 1;
-
-	setInterval(() => {
-        temp_1 += incrementor_1;
-        active_pump1.val().temp = temp_1;
-        console.log(active_pump1.val().temp);
-
-        
-        if(active_pump1 != "undefiend")
-        {
-            pump_1.set(active_pump1.val());
-        }
-	}, 1000);
 
 // utils
-function convertTime(number)
-{
-    let active_time = parseInt(number / 60) + ":" + number % 60;
-    
-    return active_time;
-}
+function convertTime(number) {
+  if (number == 0) {
+    return "0:0";
+  }
+  let active_time = parseInt(number / 60) + ":" + (number % 60);
 
+  return active_time;
+}
